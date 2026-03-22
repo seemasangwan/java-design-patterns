dc82ef7dabed2aa602fcec8cdc468e0c35771ee1
Seema <104920728+seemasangwan@users.noreply.github.com>
2026-03-22 23:38:26 +0530

    Update Main.java

--- Creational/factory/Notification/Main.java
+++ Creational/factory/Notification/Main.java
@@ -1,23 +1,10 @@
 import java.awt.image.DataBufferShort;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-/*public class Main {
+public class Main {
     public static void main(String[] args) {
         NotificationFactory notificationFactory = new NotificationFactory();
         Notification notification1 = notificationFactory.getNotification("sms", "2323", "sms message");
         Notification notification2 = notificationFactory.getNotification("email", "dff", "email message");
         notification1.sendNotification();
         notification2.sendNotification();
-    }
-}*/
-public class Main {
-    public static void main(String[] args) {
-    DatabaseFactoryProvider factoryProvider=new DatabaseFactoryProvider();
-    DatabaseFactory databaseFactory=factoryProvider.getDatabaseFactory("mongo");
-    DatabaseConnection databaseConnection=databaseFactory.getDatabaseConnection();
-    DatabaseQueryExecutor queryExecutor= databaseFactory.getDatabaseQueryExecutor();
-    databaseConnection.createConnection();
-    queryExecutor.executeQuery();
     }
 }
